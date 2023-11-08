fb7749dbec528253d325b0fc04afdb0e239a528e
fedcal <gunere3@gmail.com>
2023-11-08 22:14:59 +0100

    stuff

--- University/Bg/wordcount/src/main/java/com/example/WC_Runner.java
+++ University/Bg/wordcount/src/main/java/com/example/WC_Runner.java
@@ -9,6 +9,8 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.chain.ChainMapper;
+import org.apache.hadoop.mapreduce.lib.chain.ChainReducer;
 
 public class WC_Runner {
 
@@ -22,7 +24,7 @@ public class WC_Runner {
         Job job = Job.getInstance(conf, "WordCount");
         job.setJarByClass(WC_Runner.class);
         job.setMapperClass(WC_Mapper.class);
-        // job.setCombinerClass(WC_Reducer.class);
+        job.setCombinerClass(WC_Reducer.class);
         job.setReducerClass(WC_Reducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
@@ -30,6 +32,33 @@ public class WC_Runner {
         FileOutputFormat.setOutputPath(job, new Path(WC_Runner.OUTPUT));
         job.waitForCompletion(true);
     }
+
+
+    public static void main2(String[] args) throws Exception {
+        org.apache.log4j.BasicConfigurator.configure();
+        WC_Runner.deleteOutput(new File(WC_Runner.OUTPUT));
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "WordCount");
+        job.setJarByClass(WC_Runner.class);
+        
+        Configuration mapConf1 = new Configuration(false);
+        ChainMapper.addMapper(job, WC_Mapper.class, Object.class, Text.class, Text.class, IntWritable.class, mapConf1);
+
+        Configuration mapConf2 = new Configuration(false);
+        ChainMapper.addMapper(job, ID_Mapper.class, Text.class, IntWritable.class, Text.class, IntWritable.class, mapConf2);
+        
+        Configuration redConf1 = new Configuration(false);
+        ChainReducer.setReducer(job, WC_Reducer.class, Text.class, IntWritable.class, Text.class, IntWritable.class, redConf1);
+        // ChainReducer.addMapper(job, InverseMapper.class, Text.class, IntWritable.class, IntWritable.class, Text.class, null);
+        // job.setCombinerClass(WC_Reducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(WC_Runner.INPUT));
+        FileOutputFormat.setOutputPath(job, new Path(WC_Runner.OUTPUT));
+        job.waitForCompletion(true);
+    }
+
 
     public static void deleteOutput(File folder) {
         File[] files = folder.listFiles();
